8eacf4521432a01405391c924f76a8d32cc7bea7
mirty <mirtyricciolina@hotmail.it>
2017-02-24 11:29:05 +0100

    aggiornamenti fra

--- app/src/main/java/com/example/andrea/boc_bitsofcode/Livello2medio.java
+++ app/src/main/java/com/example/andrea/boc_bitsofcode/Livello2medio.java
@@ -1,12 +1,15 @@
 package com.example.andrea.boc_bitsofcode;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.Vibrator;
 
 import java.util.Random;
 
@@ -16,7 +19,7 @@ import java.util.Random;
 
 public class Livello2medio extends AppCompatActivity {
     Button interi, decimali, stringhe, booleani;
-    TextView quesito;
+    TextView quesito, consegna;
     public static boolean daActivity = false;
     private GeneraQuesiti gq = new GeneraQuesiti();
     private Quesito q = new Quesito("", "");
@@ -24,7 +27,9 @@ public class Livello2medio extends AppCompatActivity {
     private int daIndovinare = 10, serie=0;
     static int punteggio, giusti = 0;
     private Vibrator v;
+    CountDownTimer timer;
     Random r = new Random();
+    Typeface button_font, consegne_font;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -35,8 +40,31 @@ public class Livello2medio extends AppCompatActivity {
         decimali = (Button) findViewById(R.id.decimali);
         booleani = (Button) findViewById(R.id.booleani);
         stringhe = (Button) findViewById(R.id.stringhe);
+        consegna = (TextView) findViewById(R.id.consegnaliv2medio);
 
         quesito = (TextView) findViewById(R.id.quesito);
+
+        button_font = Typeface.createFromAsset(getAssets(),  "fonts/Acids!.otf");
+        consegne_font = Typeface.createFromAsset(getAssets(),  "fonts/DK The Cats Whiskers.otf");
+        interi.setTypeface(button_font);
+        decimali.setTypeface(button_font);
+        booleani.setTypeface(button_font);
+        stringhe.setTypeface(button_font);
+        quesito.setTypeface(consegne_font);
+        consegna.setTypeface(consegne_font);
+
+        timer = new CountDownTimer(500, 100) { //Inizializzo il timer per la gestione del cambiamento di colore
+            @Override
+            public void onTick(long millisUntilFinished) {
+                quesito.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onFinish() {
+                quesito.setTextColor(Color.parseColor("#00838F"));
+            }
+        };
+
 
         q = gq.genera();
         quesito.setText(q.getQuesito());
@@ -55,6 +83,7 @@ public class Livello2medio extends AppCompatActivity {
                         startActivity(new Intent(Livello2medio.this, Vittoria2media.class));
                     }
                 } else {
+                    timer.start();
                     v.vibrate(500);
                     serie=0;
                 }
@@ -74,6 +103,7 @@ public class Livello2medio extends AppCompatActivity {
                         startActivity(new Intent(Livello2medio.this, Vittoria2media.class));
                     }
                 } else {
+                    timer.start();
                     v.vibrate(500);
                     serie=0;
                 }
@@ -93,6 +123,7 @@ public class Livello2medio extends AppCompatActivity {
                         startActivity(new Intent(Livello2medio.this, Vittoria2media.class));
                     }
                 } else {
+                    timer.start();
                     v.vibrate(500);
                     serie=0;
                 }
@@ -112,6 +143,7 @@ public class Livello2medio extends AppCompatActivity {
                         startActivity(new Intent(Livello2medio.this, Vittoria2media.class));
                     }
                 } else {
+                    timer.start();
                     v.vibrate(500);
                     serie=0;
                 }
